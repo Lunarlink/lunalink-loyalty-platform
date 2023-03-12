dfe32f53f9e0aea1044386474b27248cee94b22c
Mijura <miodragvilotijevic@gmail.com>
2023-03-12 14:40:56 +0100

    theme creation

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,35 @@ import './index.css';
 import Home from './pages/HomePage';
 import ProgramCreation from './pages/ProgramCreation';
 import ProgramView from './pages/ProgramView';
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/program-creation">Program Creation</Link>
-          </nav>
-          <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/program-creation" Component={ProgramCreation} />
-            <Route path="/program-view" Component={ProgramView} />
-          </Routes>
-        </div>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>
+              <div>
+                <Button variant="outlined" onClick={() => {
+                  alert('clicked');
+                }}>Program Creation</Button>
+              </div>
+            </nav>
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="/program-creation" Component={ProgramCreation} />
+              <Route path="/program-view" Component={ProgramView} />
+            </Routes>
+          </div>
+        </Router>
+      </ThemeProvider>
+
     );
   }
 }
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      background: {
+        default: "#11111F"
+      },
+      primary: {
+        main: '#3CDBD3'
+      },
+    },
+  })
+
+  export default theme;
